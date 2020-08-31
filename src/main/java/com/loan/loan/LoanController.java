b84bdb8e4ae6ceae163f45db3ff9ad3877dfeda5
Adilson Lima <47046320+adilsonl@users.noreply.github.com>
2020-08-31 17:17:10 -0300

    helo World

--- src/main/java/com/loan/loan/LoanController.java
+++ src/main/java/com/loan/loan/LoanController.java
@@ -28,7 +28,12 @@ import models.User;
 public class LoanController {
 
 	public static final String COL_NAME = "loans";
- 
+	
+	
+	@GetMapping("/")
+	public String helloWorld() {
+		return "Hello World";
+	}
 	@PostMapping("/addLoan")
 	public String addLoan(@RequestBody Loan loan) throws InterruptedException, ExecutionException {
 		Firestore dbFirestore = FirestoreClient.getFirestore();
